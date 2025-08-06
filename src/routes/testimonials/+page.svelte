32cc75e03f2f7b633778e25011c57105b7e204f3
Erik Dunning <erikdunning@gmail.com>
2025-08-06 07:15:17 -0500

    feat: testimonials

--- src/routes/testimonials/+page.svelte
+++ src/routes/testimonials/+page.svelte
@@ -1,13 +1,25 @@
-<script lang="ts">
+<script lang="ts" module>
 	import Introduction from '$lib/content/testimonials/Introduction.svx'
 	import HeadMetas from '$lib/components/HeadMetas.svelte'
+	import EdPritchard from '$lib/content/testimonials/quotes/ed-pritchard.svx'
+	import SwanandKavitkar from '$lib/content/testimonials/quotes/swanand-kavitkar.svx'
+	import StevenBoutcher from '$lib/content/testimonials/quotes/steven-boutcher.svx'
+
+	const quotes = [EdPritchard, SwanandKavitkar, StevenBoutcher]
 </script>
 
 <HeadMetas
 	title="Testimonials"
-	description="Testimonials from clients and colleagues about my work and abilities. Their feedback highlights my skills, professionalism, and the impact of my projects."
+	description="Testimonials from clients and colleagues about my work and abilities. Their feedback highlight my skills, professionalism, and the impact of my efforts on projects."
 />
 
 <article class="max-w-5xl w-full mx-auto px-6 prose">
 	<Introduction />
+	<div class="flex flex-col gap-6 my-20">
+		{#each quotes as Quote}
+			<div class="quote-container">
+				<Quote />
+			</div>
+		{/each}
+	</div>
 </article>
